c7845ec0c4b6132d415f4775e40a4f68bdd51655
Toggy Smith <toggysmith@gmail.com>
2023-03-04 20:07:59 +0000

    feat: create a core app

--- meridio/meridio/settings.py
+++ meridio/meridio/settings.py
@@ -39,13 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Meridio
+    'core',
     'accounts',
-]
-
-TAILWIND_APP_NAME = "theme"
-
-INTERNAL_IPS = [
-    "127.0.0.1",
 ]
 
 MIDDLEWARE = [
